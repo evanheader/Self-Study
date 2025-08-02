08a7ccd04f601c93da051f6f494a4ec72407fac0
evanheader <evanheader@gmail.com>
2025-08-02 16:43:10 -0700

    Finished python program

--- Python/project1/src/countdown_timer.py
+++ Python/project1/src/countdown_timer.py
@@ -1,2 +1,17 @@
 # Python 
 # For Loop Practice
+
+import time
+
+timer = int(input("Enter the amount of seconds to wait: "))
+
+for x in range(timer, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+    
+print("Timer is up")
+
+    
