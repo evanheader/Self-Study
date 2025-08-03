2abb7e051f373c3f1bb4fea0275506d0bfdacb63
evanheader <evanheader@gmail.com>
2025-08-03 09:57:32 -0700

    Implemented splitting and double options. Prints correct action into terminal

--- Python/blackjack_practice/game.py
+++ Python/blackjack_practice/game.py
@@ -101,7 +101,9 @@ class Hand:
             print(x)
             
     def isPair(self):
-        if(self.cards[0].value == self.cards[1].value):
+        if(len(self.cards) > 2):
+            return False
+        elif(self.cards[0].value == self.cards[1].value):
             return True
         else:
             return False
@@ -131,7 +133,11 @@ def draw_player_hand(player):
         screen.blit(img, (50 + i*30,100 + i*10))
         
 def draw_dealer_hand(dealer, player=True):
-    draw_text(f"Dealer Hand: {dealer.value}",640,50)
+    if player:
+        value = dealer.cards[0].value
+    else:
+        value = dealer.value
+    draw_text(f"Dealer Hand: {value}",640,50)
     for i, card in enumerate(dealer.cards):
         if(player and i == 0):
             img = card.get_image()
@@ -141,6 +147,92 @@ def draw_dealer_hand(dealer, player=True):
         elif(not player):
             img = card.get_image()
             screen.blit(img, (640 + i*110,100))
+            
+def should_split(card, dealer):
+    if(card.rank == 'A'):
+        return True
+    if(card.value == 10):
+        return False
+    if(card.rank == '9'):
+        if dealer.rank not in ['7', 'J', 'Q','K', 'A']:
+            return True
+        return False
+    if(card.rank == '8'):
+        return True
+    if(card.rank == '7'):
+        if dealer.rank not in ['8','9', 'J', 'Q','K', 'A']:
+            return True
+        return False
+    if(card.rank == '6'):
+        if dealer.rank not in ['7','8','9', 'J', 'Q','K', 'A']:
+            return True
+        return False
+    if(card.rank == '5'):
+        return False
+    if(card.rank == '4'):
+        if dealer.rank in ['5','6']:
+            return True
+        return False
+    if(card.rank == '3'):
+        if dealer.rank not in ['8','9', 'J', 'Q','K', 'A']:
+            return True
+        return False
+    if(card.rank == '2'):
+        if dealer.rank not in ['8','9', 'J', 'Q','K', 'A']:
+            return True
+        return False
+    return False
+
+def soft_action(value, dealer):
+    if value == 9:
+        return "S"
+    if value == 8:
+        if dealer.rank in ['6']:
+            return "D"
+        return "S"
+    if value == 7:
+        if dealer.rank in ['2','3','4','5','6']:
+            return "D"
+        if dealer.rank in ['7','8']:
+            return "S"
+        return "H"
+    if value == 6:
+        if dealer.rank in ['3','4','5','6']:
+            return "D"
+        return "H"
+    if value in [5, 4]:
+        if dealer.rank in ['4','5','6']:
+            return "D"
+        return "H"
+    if value in [3, 2]:
+        if dealer.rank in ['5','6']:
+            return "D"
+        return "H"
+    
+def hard_action(value, dealer):
+    if value >= 17:
+        return "S"
+    if value < 17 and value > 12:
+        if dealer.rank in ['2','3','4','5','6']:
+            return "S"
+        return "H"
+    if value == 12:
+        if dealer.rank in ['4','5','6']:
+            return "S"
+        return "H"
+    if value == 11:
+        return "D"
+    if value == 10:
+        if dealer.rank in ['J','Q','K','A']:
+            return "H"
+        return "D"
+    if value == 9:
+        if dealer.rank in ['3','4','5','6']:
+            return "D"
+        return "H"
+    if value <= 8:
+        return "H"
+    
 
 # Game State
 deck = Deck()
@@ -154,11 +246,19 @@ playing = False
 player = False
 round_over = False
 can_split = False
+can_double = False
+
+action = ""
 
 running = True
 while running:
     screen.fill(GREEN)
     
+    if(len(player_hand.cards) <= 2):
+        can_double = True
+    else:
+        can_double = False
+        
     
     for event in pygame.event.get():
         
@@ -176,13 +276,41 @@ while running:
                 clear_table(player_hand, dealer_hand)
                 deal(player_hand, dealer_hand)
                 
+                action = ""
+                
                 if(player_hand.isPair()):
                     can_split = True
+                    if should_split(player_hand.cards[0], dealer_hand.cards[0]):
+                        action = "split"
+                
+                value_without_aces = 0
+                for card in player_hand.cards:
+                    if card.rank != 'A':
+                        value_without_aces += card.value
+                
+                if(player_hand.aces > 0 and not action):
+                    if soft_action(value_without_aces, dealer_hand.cards[0]) == 'D':
+                        action = "double"
+                    if soft_action(value_without_aces, dealer_hand.cards[0]) == 'H':
+                        action = "hit"
+                    if soft_action(value_without_aces, dealer_hand.cards[0]) == 'S':
+                        action = "stand"
+                        
+                if not action:
+                    if hard_action(player_hand.value, dealer_hand.cards[0]) == 'D':
+                        action = "double"
+                    if hard_action(player_hand.value, dealer_hand.cards[0]) == 'H':
+                        action = "hit"
+                    if hard_action(player_hand.value, dealer_hand.cards[0]) == 'S':
+                        action = "stand"
+
+                print(action)
                 
                 if(dealer_hand.value == 21 or player_hand.value == 21):
                     playing = False
                     player = False
                     round_over = True
+                    pygame.display.flip()
     
             elif playing:
                 if hit_button.collidepoint(mouse_pos):
@@ -191,7 +319,37 @@ while running:
                         player = False
                         playing = False
                         round_over = True
+                        can_split = False
                         continue
+                    
+                    action = ""
+                    
+                    value_without_aces = 0
+                    for card in player_hand.cards:
+                        if card.rank != 'A':
+                            value_without_aces += card.value
+                    
+                    if(player_hand.aces > 0 and not action):
+                        if soft_action(value_without_aces, dealer_hand.cards[0]) == 'D':
+                            if value_without_aces in [7, 8]:
+                                action = "stand"
+                            else:
+                                action = "hit"
+                        if soft_action(value_without_aces, dealer_hand.cards[0]) == 'H':
+                            action = "hit"
+                        if soft_action(value_without_aces, dealer_hand.cards[0]) == 'S':
+                            action = "stand"
+                            
+                    if not action:
+                        if hard_action(player_hand.value, dealer_hand.cards[0]) == 'D':
+                            action = "hit"
+                        if hard_action(player_hand.value, dealer_hand.cards[0]) == 'H':
+                            action = "hit"
+                        if hard_action(player_hand.value, dealer_hand.cards[0]) == 'S':
+                            action = "stand"
+
+                    print(action)
+                    
                 elif stand_button.collidepoint(mouse_pos):
                     player = False
                     while(dealer_hand.value < 17 or (dealer_hand.value == 17 and dealer_hand.aces > 0)):
@@ -203,15 +361,67 @@ while running:
                         dealer_hand.add_card(deck.deal())
                     round_over = True
                     playing = False
+                    can_split = False
+                elif can_split and split_button.collidepoint(mouse_pos):
+                    pass
+                elif double_button.collidepoint(mouse_pos):
+                    player_hand.add_card(deck.deal())
+                    player = False
+                    if(player_hand.value > 21):
+                        round_over = True
+                        playing = False
+                        can_split = False
+                        round_over = True
+                        continue
+                    while(dealer_hand.value < 17 or (dealer_hand.value == 17 and dealer_hand.aces > 0)):
+                        screen.fill(GREEN)
+                        draw_dealer_hand(dealer_hand,player)
+                        draw_player_hand(player_hand)
+                        pygame.display.flip()
+                        time.sleep(0.5)
+                        dealer_hand.add_card(deck.deal())
+                    playing = False
+                    can_split = False
+                    round_over = True
+                    
                     
             
             elif round_over:
                 if again_button.collidepoint(mouse_pos):
+                    action = ""
                     round_over = False
                     playing = True
                     player = True
                     clear_table(player_hand, dealer_hand)
                     deal(player_hand, dealer_hand)
+                    
+                    if(player_hand.isPair()):
+                        can_split = True
+                        if should_split(player_hand.cards[0], dealer_hand.cards[0]):
+                            action = "split"
+                    
+                    value_without_aces = 0
+                    for card in player_hand.cards:
+                        if card.rank != 'A':
+                            value_without_aces += card.value
+                    
+                    if(player_hand.aces > 0 and not action):
+                        if soft_action(value_without_aces, dealer_hand.cards[0]) == 'D':
+                            action = "double"
+                        if soft_action(value_without_aces, dealer_hand.cards[0]) == 'H':
+                            action = "hit"
+                        if soft_action(value_without_aces, dealer_hand.cards[0]) == 'S':
+                            action = "stand"
+                            
+                    if not action:
+                        if hard_action(player_hand.value, dealer_hand.cards[0]) == 'D':
+                            action = "double"
+                        if hard_action(player_hand.value, dealer_hand.cards[0]) == 'H':
+                            action = "hit"
+                        if hard_action(player_hand.value, dealer_hand.cards[0]) == 'S':
+                            action = "stand"
+
+                    print(action)
                     
                     if(dealer_hand.value == 21 or player_hand.value == 21):
                         playing = False
@@ -234,9 +444,14 @@ while running:
         if(player):
             hit_button = draw_button("Hit", 100, 500, 100, 40)
             stand_button = draw_button("Stand", 220, 500, 100, 40)
+            if(can_double):
+                double_button = draw_button("Double", 220, 550, 100, 40)
+            if(can_split):
+                split_button = draw_button("Split", 100, 550, 100, 40)
+            
             
     if(round_over):
-        again_button = draw_button("Again", 640, 410, 100, 40)
+        again_button = draw_button("Again", 100, 600, 100, 40)
         
     
     
